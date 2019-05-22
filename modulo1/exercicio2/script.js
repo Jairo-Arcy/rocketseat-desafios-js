537226744eb51c789efce446895ff2eb86943f19
Jairo Arcy Tunisse <jairo_tunisse@outlook.com>
2019-05-22 16:43:33 -0300

    add module 2

---
+++ modulo1/exercicio2/script.js
@@ -0,0 +1,10 @@
+function pares(n1, n2){
+  for(var i = n1; i <= n2; i++) {
+    if(i % 2 === 0){
+      console.log(i);
+
+    }
+
+  }
+  
+}
---
+++ modulo1/exercicio3/script.js
@@ -0,0 +1,13 @@
+function temHabilidade(skills){
+  if(skills.indexOf("JavaScript") > -1){
+    skills.push("JavaScript");
+    return true;
+  }else {
+    return false;
+
+  }
+  
+}
+var skills = ["JavaScript", "React", "CSS"];
+
+
